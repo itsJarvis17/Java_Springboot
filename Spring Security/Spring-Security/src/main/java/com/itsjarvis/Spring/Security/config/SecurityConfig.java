19c8eae923762a5135d985d37981a72d0293dda8
itsJarvis17 <bhargav.naware@gmail.com>
2024-11-21 20:30:14 +0530

    added user and admin in spring security config

--- Spring Security/Spring-Security/src/main/java/com/itsjarvis/Spring/Security/config/SecurityConfig.java
+++ Spring Security/Spring-Security/src/main/java/com/itsjarvis/Spring/Security/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,15 @@ public class SecurityConfig {
        // http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    // Working with multiple in memory users
+    @Bean
+    public UserDetailsService createUsers() {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user@123").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin@123").roles("ADMIN").build();
+
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
