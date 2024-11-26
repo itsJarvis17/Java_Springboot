d930e349e68bf760b3666f15de0492c5118b1761
itsJarvis17 <bhargav.naware@gmail.com>
2024-11-26 19:13:01 +0530

    Added code for authenticating user using encrypted password

--- Spring Security/Spring-Security/src/main/java/com/itsjarvis/Spring/Security/config/SecurityConfig.java
+++ Spring Security/Spring-Security/src/main/java/com/itsjarvis/Spring/Security/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.itsjarvis.Spring.Security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,11 +22,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+
+        // Create instance of who provides authentication in this case database
+        DaoAuthenticationProvider daoAuthProvider = new DaoAuthenticationProvider();
+
+        // pass userdetails service instance to dao authentication provider to check
+        daoAuthProvider.setUserDetailsService(userDetailsService);
+        daoAuthProvider.setPasswordEncoder(passwordEncoder());
+        // return authentication provider instance
+        return daoAuthProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(customizer -> customizer.disable());// disable csrf
-        http.authorizeHttpRequests(authorize->authorize.anyRequest().authenticated());// any request should be authenticated
+        http.authorizeHttpRequests(authorize->authorize.requestMatchers("/login","/register").permitAll().anyRequest().authenticated());// any request should be authenticated
         http.formLogin(Customizer.withDefaults());  // use default form login & can't be used when request is stateless
         http.httpBasic(Customizer.withDefaults()); // allows to pass user login creds through HTTP request header
        // http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -28,13 +50,18 @@ public class SecurityConfig {
         return http.build();
     }
 
-    // Working with multiple in memory users
     @Bean
-    public UserDetailsService createUsers() {
-        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user@123").roles("USER").build();
-        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin@123").roles("ADMIN").build();
-
-
-        return new InMemoryUserDetailsManager(user, admin);
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
+
+    // Working with multiple in memory users
+//    @Bean
+//    public UserDetailsService createUsers() {
+//        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("user@123").roles("USER").build();
+//        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin@123").roles("ADMIN").build();
+//
+//
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
