1dc308568f52b22ee419b17feea84fd12fa99d19
itsJarvis17 <bhargav.naware@gmail.com>
2024-12-02 12:06:32 +0530

    Added JWT token generation and validation code

--- JarvisEcomWebsite/JarvisEcomWebsite/src/main/java/com/itsjarvis/JarvisEcomWebsite/filter/JwtFilter.java
+++ JarvisEcomWebsite/JarvisEcomWebsite/src/main/java/com/itsjarvis/JarvisEcomWebsite/filter/JwtFilter.java
@@ -1,9 +1,7 @@
-package com.itsjarvis.JarvisEcomWebsite.config;
+package com.itsjarvis.JarvisEcomWebsite.filter;
 
 import com.itsjarvis.JarvisEcomWebsite.service.CustomUserDetailsService;
 import com.itsjarvis.JarvisEcomWebsite.service.JwtService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,8 +11,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
